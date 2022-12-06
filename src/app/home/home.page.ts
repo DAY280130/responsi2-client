7817df6ea2748a39eccb9ddd85d9c8f6e02dfa9a
DAY280130 <dimas.280130@gmail.com>
2022-12-06 20:33:17 +0700

    edit and delete done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,21 +11,36 @@ import { ContactService } from '../services/contact.service';
 export class HomePage implements OnInit {
   user: string = '';
   isModalOpen: boolean = false;
+  id: string = '';
   name: string = '';
   number: string = '';
+  mode: string = '';
+  contacts: any[] = [];
 
   constructor(private acc: AccountService, private cont: ContactService) {}
 
   async ngOnInit(): Promise<void> {
     this.user = (await Preferences.get({ key: 'user' })).value || '';
+    this.cont.getContacts().subscribe((res) => {
+      this.contacts = res.data.contacts;
+      console.log(this.contacts);
+    });
   }
 
   logout() {
     this.acc.logout();
   }
 
-  openModal(mode: string) {
+  async openModal(mode: string, id: string) {
+    this.mode = mode;
     this.isModalOpen = true;
+    if (mode === 'edit') {
+      this.cont.getContact(id).subscribe((res) => {
+        this.id = res.data.contact.id;
+        this.name = res.data.contact.name;
+        this.number = res.data.contact.number;
+      });
+    }
   }
 
   closeModal() {
@@ -36,5 +51,21 @@ export class HomePage implements OnInit {
 
   addContact() {
     this.cont.addContact(this.name, this.number);
+  }
+
+  submit() {
+    if (this.mode == 'add') {
+      this.addContact();
+    } else {
+      this.editContact(this.id);
+    }
+  }
+
+  editContact(id: string) {
+    this.cont.editContact(id, this.name, this.number);
+  }
+
+  deleteContact(id: string) {
+    this.cont.deleteContact(id);
   }
 }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { promises } from 'dns';
+import { from, Observable } from 'rxjs';
 import { ApiService } from './api.service';
 
 @Injectable({
@@ -9,7 +11,7 @@ export class ContactService {
   endpoint: string = 'contacts';
   constructor(private api: ApiService, private alertCtrl: AlertController) {}
 
-  addContact(name: string, number: string) {
+  addContact(name: string, number: string): void {
     this.api
       .doPost(`${this.endpoint}/add`, { name, number })
       .subscribe((res) => {
@@ -37,5 +39,88 @@ export class ContactService {
             .then((res) => res.present());
         }
       });
+  }
+
+  getContacts(): Observable<any> {
+    return from(this.api.doGet(`${this.endpoint}/getall`, null));
+  }
+
+  getContact(id: string): Observable<any> {
+    return from(this.api.doGet(`${this.endpoint}/get`, id));
+  }
+
+  editContact(id: string, name: string, number: string): void {
+    this.api
+      .doPut(`${this.endpoint}/edit`, { id, name, number })
+      .subscribe((res) => {
+        // console.log(res);
+        if (res.data.update_status === 'success') {
+          this.alertCtrl
+            .create({
+              header: 'Notifikasi',
+              subHeader: 'Berhasil Mengubah Kontak!',
+              buttons: [
+                {
+                  text: 'OK',
+                  handler: () => location.reload(),
+                },
+              ],
+            })
+            .then((res) => res.present());
+        } else {
+          this.alertCtrl
+            .create({
+              header: 'Notifikasi',
+              subHeader: 'Gagal Mengubah Kontak!',
+              buttons: ['OK'],
+            })
+            .then((res) => res.present());
+        }
+      });
+  }
+
+  deleteContact(id: string) {
+    this.alertCtrl
+      .create({
+        header: 'Perhatian',
+        subHeader: 'Yakin ingin menghapus kontak?',
+        buttons: [
+          {
+            text: 'TIDAK',
+          },
+          {
+            text: 'YAKIN',
+            handler: () => {
+              this.api
+                .doDelete(`${this.endpoint}/remove`, { id })
+                .subscribe((res) => {
+                  if (res.data.delete_status === 'success') {
+                    this.alertCtrl
+                      .create({
+                        header: 'Notifikasi',
+                        subHeader: 'Berhasil Menghapus Kontak!',
+                        buttons: [
+                          {
+                            text: 'OK',
+                            handler: () => location.reload(),
+                          },
+                        ],
+                      })
+                      .then((res) => res.present());
+                  } else {
+                    this.alertCtrl
+                      .create({
+                        header: 'Notifikasi',
+                        subHeader: 'Gagal Menghapus Kontak!',
+                        buttons: ['OK'],
+                      })
+                      .then((res) => res.present());
+                  }
+                });
+            },
+          },
+        ],
+      })
+      .then((res) => res.present());
   }
 }
